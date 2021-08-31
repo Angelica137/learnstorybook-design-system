258c91d50049d024b827f93267bdf89f8c137526
Angelica Beristain <angelica.beristain@shplc-h2s7.mynet>
2021-08-31 12:19:58 +0100

    update avatar doc

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -5,6 +5,9 @@ import { Avatar } from "./Avatar";
 export default {
   title: "Design System/Avatar",
   component: Avatar,
+  parameters: {
+    componentSubtitle: "Display an image that represents a user or organisation",
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
